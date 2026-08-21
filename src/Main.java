bb70cd238a7b4f4388b7380d3b032e19eabb68f0
Unai <irunavarro@lhusurbil.eus>
2026-08-21 08:04:02 +0200

    Koloreen erabilera inprimatutako testuan

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,14 @@ public class Main{
     public static final String RESET= "\u001B[0m";
     public static final String KOLORE1= "\u001B[31m";
     public static final String KOLORE2= "\u001B[30m";
+    public static final String KOLORE3="\u001B[29m";
     public static void main (String[] args){
         
         System.out.println(BERDEA+"Lehenengo testua idazten"+RESET);
         System.out.println(BERDEA+"Bigarren testua idazten"+RESET);
         System.out.println(KOLORE1+"Hirugarren testua"+RESET);
         System.out.println(KOLORE2+"Laugarren testua"+RESET);
+        System.out.println(KOLORE3+"Bostgarren testua idazten"+RESET);
 
     }
 }
